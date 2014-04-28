fe1d6102fabbc4968c13d9bab37903b2d3d217a5
Justin Yoo <justin.yoo@aliencube.com>
2014-04-28 11:03:50 +1000

    Added XML comments for exception classes.

--- Aliencube.CryptoService/Exceptions/KeyNotFoundException.cs
+++ Aliencube.CryptoService/Exceptions/KeyNotFoundException.cs
@@ -3,26 +3,46 @@ using System.Runtime.Serialization;
 
 namespace Aliencube.CryptoService.Exceptions
 {
-	public class KeyNotFoundException : ApplicationException
-	{
-		public KeyNotFoundException()
-			: base()
-		{
-		}
+    /// <summary>
+    /// This represents an exception entity to be thrown when a key is not found.
+    /// </summary>
+    public class KeyNotFoundException : ApplicationException
+    {
+        /// <summary>
+        /// Initialises a new instance of the KeyNotFoundException class.
+        /// </summary>
+        public KeyNotFoundException()
+            : base()
+        {
+        }
 
-		public KeyNotFoundException(SerializationInfo info, StreamingContext context)
-			: base(info, context)
-		{
-		}
+        /// <summary>
+        /// Initialises a new instance of the KeyNotFoundException class.
+        /// </summary>
+        /// <param name="info">The object that holds the serialized object data.</param>
+        /// <param name="context">The contextual information about the source or destination.</param>
+        public KeyNotFoundException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
 
-		public KeyNotFoundException(string message)
-			: base(message)
-		{
-		}
+        /// <summary>
+        /// Initialises a new instance of the KeyNotFoundException class.
+        /// </summary>
+        /// <param name="message">A message that describes the error.</param>
+        public KeyNotFoundException(string message)
+            : base(message)
+        {
+        }
 
-		public KeyNotFoundException(string message, Exception innerException)
-			: base(message, innerException)
-		{
-		}
-	}
+        /// <summary>
+        /// Initialises a new instance of the KeyNotFoundException class.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception. If the innerException parameter is not a null reference, the current exception is raised in a catch block that handles the inner exception.</param>
+        public KeyNotFoundException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
 }
--- Aliencube.CryptoService/Exceptions/ValueNotFoundException.cs
+++ Aliencube.CryptoService/Exceptions/ValueNotFoundException.cs
@@ -3,26 +3,46 @@ using System.Runtime.Serialization;
 
 namespace Aliencube.CryptoService.Exceptions
 {
-	public class ValueNotFoundException : ApplicationException
-	{
-		public ValueNotFoundException()
-			: base()
-		{
-		}
+    /// <summary>
+    /// This represents an exception entity to be thrown when a value is not found.
+    /// </summary>
+    public class ValueNotFoundException : ApplicationException
+    {
+        /// <summary>
+        /// Initialises a new instance of the ValueNotFoundException class.
+        /// </summary>
+        public ValueNotFoundException()
+            : base()
+        {
+        }
 
-		public ValueNotFoundException(SerializationInfo info, StreamingContext context)
-			: base(info, context)
-		{
-		}
+        /// <summary>
+        /// Initialises a new instance of the ValueNotFoundException class.
+        /// </summary>
+        /// <param name="info">The object that holds the serialized object data.</param>
+        /// <param name="context">The contextual information about the source or destination.</param>
+        public ValueNotFoundException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
 
-		public ValueNotFoundException(string message)
-			: base(message)
-		{
-		}
+        /// <summary>
+        /// Initialises a new instance of the ValueNotFoundException class.
+        /// </summary>
+        /// <param name="message">A message that describes the error.</param>
+        public ValueNotFoundException(string message)
+            : base(message)
+        {
+        }
 
-		public ValueNotFoundException(string message, Exception innerException)
-			: base(message, innerException)
-		{
-		}
-	}
+        /// <summary>
+        /// Initialises a new instance of the ValueNotFoundException class.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception. If the innerException parameter is not a null reference, the current exception is raised in a catch block that handles the inner exception.</param>
+        public ValueNotFoundException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
 }
