349868637e30febe5959c56fc22b03378fbc22b5
Justin Yoo <justin.yoo@aliencube.com>
2014-04-30 23:45:42 +1000

    Added downloads.html. Updated main.js.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,33 @@
-console.log('This would be the main JS file.');
+;"use strict";
+(function($){
+	var getSha = function() {
+		var url = "https://api.github.com/repos/aliencube/CryptoService/git/refs/heads/gh-pages";
+		$.ajax({
+		    type: "GET",
+		    url: url,
+			dataType: "json"
+		})
+		.done(function(data) {
+			getBuilds(data.object.sha);
+		});
+	};
+	
+	var getBuilds = function(sha) {
+		// var url = "";
+		// $.ajax({
+		    // type: "GET",
+		    // url: url,
+			// dataType: "json"
+		// })
+		// .done(function(data) {
+			// getBuilds(data.object.sha);
+		// });
+	};
+
+	$(document).ready(function() {
+		getSha();
+	
+		$("#latest").html("<li>HelloWorld</li>");
+		$("#archived").html("<li>Archived</li>");
+	});
+})(jQuery);
